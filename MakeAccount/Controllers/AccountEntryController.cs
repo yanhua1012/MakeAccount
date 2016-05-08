51094af150960078f5cadf814b3a018e4c0035b5
User <User@1512010NB01>
2016-05-08 18:46:03 +0800

    加入專案檔案。

---
+++ MakeAccount/Controllers/AccountEntryController.cs
@@ -0,0 +1,56 @@
+﻿using MakeAccount.Models;
+using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MakeAccount.Controllers
+{
+    public class AccountEntryController : Controller
+    {
+        protected static readonly ConcurrentQueue<AccountEntryModels> fakeAccounts = new ConcurrentQueue<AccountEntryModels>(
+            new[] {
+                new AccountEntryModels {
+                    Sn = 1,
+                    AccountType = EnumAccountType.Expense,
+                    Amount = 300m,
+                    KeepDate = new DateTime(2016, 1, 1),
+                    Memo = "電話費"
+                },
+                new AccountEntryModels {
+                    Sn = 2,
+                    AccountType = EnumAccountType.Expense,
+                    Amount = 1600m,
+                    KeepDate = new DateTime(2016, 1, 2),
+                    Memo = "早餐"
+                },
+                new AccountEntryModels {
+                     Sn = 3,
+                     AccountType = EnumAccountType.Expense,
+                     Amount = 800m,
+                     KeepDate = new DateTime(2016, 1, 3),
+                     Memo = "午餐"
+                 }
+             });
+
+        // GET: AccountEntry
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            var accountEntries = fakeAccounts;
+            return View(accountEntries);
+        }
+
+        [HttpPost]
+        public RedirectResult Add(AccountEntryModels newAccountEntry)
+        {
+            newAccountEntry.Sn = fakeAccounts.Max(x => x.Sn) + 1;
+            fakeAccounts.Enqueue(newAccountEntry);
+            return new RedirectResult("~/AccountEntry/Index");
+        }
+    }
+}
---
+++ MakeAccount/Models/AccountEntryModels.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MakeAccount.Models
+{
+    /// <summary>
+    /// 記帳類別:收入、支出
+    /// </summary>
+    public enum EnumAccountType { Expense, Revenue }
+
+    public class AccountEntryModels
+    {
+        /// <summary>
+        /// #
+        /// </summary>
+        public int Sn { get; set; }
+
+        [Required(ErrorMessage = "類別為必填")]
+        /// <summary>
+        /// 類別
+        /// </summary>
+        public EnumAccountType AccountType { get; set; }
+
+        [Required(ErrorMessage = "日期為必填")]
+        [DataType(DataType.Date)]
+        /// <summary>
+        /// 日期
+        /// </summary>
+        public DateTime KeepDate { get; set; }
+
+        [Required(ErrorMessage = "金額為必填")]
+        /// <summary>
+        /// 金額
+        /// </summary>
+        public Decimal Amount { get; set; }
+
+        /// <summary>
+        /// 備註
+        /// </summary>
+        public String Memo { get; set; }
+    }
+}
